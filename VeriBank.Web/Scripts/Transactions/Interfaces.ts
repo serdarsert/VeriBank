b61b1b3e070fb89498506f8e5c3f517b1e0604f3
serdarsert <serdar@serdarsert.com>
2015-11-26 10:11:03 +0200

    ITransaction interface updated. I{MethodName} interfaces added.

---
+++ VeriBank.Web/Scripts/Transactions/Interfaces.ts
@@ -0,0 +1,20 @@
+﻿namespace VeriBank.Business.Transactions {
+    export interface ITransaction extends IStart, IFetch, IConfirm, IExecute {
+    }
+
+    interface IStart {
+        Start: (request: cm.RequestMessage, response: cm.ResponseMessage) => void;
+    }
+
+    interface IFetch {
+        Fetch: (request: cm.RequestMessage, response: cm.ResponseMessage) => void;
+    }
+
+    interface IConfirm {
+        Confirm: (request: cm.RequestMessage, response: cm.ResponseMessage) => void;
+    }
+
+    interface IExecute {
+        Execute: (request: cm.RequestMessage, response: cm.ResponseMessage) => void;
+    }
+}
